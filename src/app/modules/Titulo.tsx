54804a69a389e4ea5851201ae55c7c62130ca385
josoelvieira <cvl.jv1@gmail.com>
2023-09-30 10:51:29 -0300

    udpate typescript

--- src/app/modules/Titulo.tsx
+++ src/app/modules/Titulo.tsx
@@ -1,4 +1,10 @@
-export default function Titulo(props) {
+interface TituloProps {
+    titulo: string;
+}
+
+export default function Titulo(props: TituloProps) {
+
+    
     return (
         <h1 className=" text-center text-4xl my-4">{props.titulo}</h1>
     )
